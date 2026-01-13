1515a1cf9170812831477d0efd18d2b242c10180
alssandrohrnandz <biol.alessandro@gmail.com>
2026-01-13 12:36:33 -0600

    Analisis Neutral y Seleccion separado

--- scripts/Discrete_Space_Inference/infLikelihood_mutations.R
+++ scripts/Discrete_Space_Inference/infLikelihood_mutations.R
@@ -44,7 +44,7 @@ DifussionValuesToCheck <- sort(unique(c(
   0.0000001, 0.000001, 0.000005, 0.00001, 0.00005, 
   0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1
 )))
-SelectionValuesToCheck <- seq(-0.05, 0.05, by=0.01)
+SelectionValuesToCheck <-0 #seq(-0.05, 0.05, by=0.01)
 
 # === 3. Lectura de Datos Inteligente ===
 
